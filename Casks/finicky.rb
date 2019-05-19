7dc118d75b558e20625bf317d3846175831ad8f2
John Sterling <john@johnste.se>
2019-05-19 14:23:38 +0200

    Update to finicky v2.0-rc.0

--- Casks/finicky.rb
+++ Casks/finicky.rb
@@ -1,5 +1,5 @@
 cask 'finicky' do
-  version '0.5'
+  version '2.0-rc.0'
   sha256 'a2eba88c657786056d0f867c983f31c786adfe5ecde1aa3c8df94e18c536775c'
 
   url "https://github.com/johnste/finicky/releases/download/v#{version}/Finicky.zip"
